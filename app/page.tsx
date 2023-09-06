353d99ac09511fef03b686c904dce09833db3f5b
qzeter <119956217+qzeter@users.noreply.github.com>
2023-09-06 21:15:58 +0400

    top_nachalo

---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+import s from "../src/styles/mainpage.module.scss";
+import { Jost } from 'next/font/google';
+
+export default function Home() {
+  return (
+      <header className={s.header}>
+        <div className={s.top}>
+          <div className={s.topInner}>
+            <div className={s.linksList}>
+              <a href="">Сервис</a>
+              <a href="">Сотрудничество</a>
+              <a href="">Заказать звонок</a>
+            </div>
+            <div className={s.iconsList}>
+              <a href=""><img src="images/viber.svg" alt="" /></a>
+              <a href=""><img src="images/whatsapp.svg" alt="" /></a>
+              <a href=""><img src="images/telegram.svg" alt="" /></a>
+            </div>
+            <div className={s.phoneContact}>
+              <a href="">+7 (800) 505-54-61</a>
+            </div>
+            <div className={s.iconPlusik}>
+            <a href=""><img src="images/plus.svg" alt="" /></a>
+            </div>
+          </div>
+        </div>
+      </header>
+  );
+}
